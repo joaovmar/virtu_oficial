5231ef98d772d82a9ce3260e383576d89ac139d8
joaovmar <vitormartinstz@gmail.com>
2026-05-27 15:46:00 -0300

    fix: corrige dependência da migration 0010

--- backend/core/migrations/0010_empreendimentopage_futuro_lancamento.py
+++ backend/core/migrations/0010_empreendimentopage_futuro_lancamento.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0009_alter_andamentoobra_options_alter_fotoobra_options_and_more'),
+        ('core', '0008_contatopage_fields'),
     ]
 
     operations = [
