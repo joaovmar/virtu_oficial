91ed146bffde51414abc4d5357d47f99c0440c3f
joaovmar <vitormartinstz@gmail.com>
2026-05-27 11:24:12 -0300

    fix: recria migration 0008 com dependência correta

---
+++ backend/core/migrations/0008_contatopage_fields.py
@@ -0,0 +1,41 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0006_homepage_video'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='contatopage',
+            name='secao_titulo',
+            field=models.CharField(
+                blank=True,
+                default='Vamos conversar sobre o seu futuro!',
+                max_length=200,
+                verbose_name='Título da Seção de Contato',
+            ),
+        ),
+        migrations.AddField(
+            model_name='contatopage',
+            name='horario_semana',
+            field=models.CharField(
+                blank=True,
+                default='Segunda a Sexta: 9h às 18h',
+                max_length=100,
+                verbose_name='Horário Semana',
+            ),
+        ),
+        migrations.AddField(
+            model_name='contatopage',
+            name='horario_fim_semana',
+            field=models.CharField(
+                blank=True,
+                default='Sábado: 9h às 13h',
+                max_length=100,
+                verbose_name='Horário Fim de Semana',
+            ),
+        ),
+    ]
